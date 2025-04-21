3bd7009e340484ae1cfd63df145681a012b2f816
Jayesh Sutar <jayeshsutar1108@gmail.com>
2025-04-21 17:55:49 +0530

    Skills section added

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,9 @@
 /*=============== MENU ===============*/
 const navMenu = document.getElementById('nav-menu'),
-navToggle = document.getElementById('nav-toggle');
+    navToggle = document.getElementById('nav-toggle');
 
 /* Menu show - hidden */
-navToggle.addEventListener('click',()=>{
+navToggle.addEventListener('click', () => {
     navMenu.classList.toggle('show-menu');
     navToggle.classList.toggle('animate-toggle');
 })
@@ -14,7 +14,21 @@ navToggle.addEventListener('click',()=>{
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
 
 /*=============== SERVICES SWIPER ===============*/
-
+var servicesSwiper = new Swiper('.services-swiper', {
+    spaceBetween: 32,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    }, 
+    breakpoints: {
+        768: {
+            slidesPerView: 2,
+        },
+        1024: {
+            slidesPerView: 3,
+        },
+    },
+});
 /*=============== MIXITUP FILTER PORTFOLIO ===============*/
 
 /* Active work */
